91ceba0b0dbc28afd6dee7a56a03ecc1366ee811
Icaro-Salgado <icaro.salgado@mercadolivre.com>
2022-03-17 19:08:59 -0300

    refact: refatora PT1PI1

--- src/main/java/br/com/mercadolivre/java_spring/parte_01/pratica_integradora_02/util/morsecode_decodifier/MorseCodeDecodifier.java
+++ src/main/java/br/com/mercadolivre/java_spring/parte_01/pratica_integradora_02/util/morsecode_decodifier/MorseCodeDecodifier.java
@@ -10,19 +10,8 @@ public class MorseCodeDecodifier {
     public static String decode(String message) {
         // Cada palavra é splitada por três espaços
         String regex = "\\s{3}";
-
         List<String> morseWords = Arrays.asList(message.split(regex));
 
-        return decodeMorseSentence(morseWords);
-    }
-
-    private static String decodeMorseSentence(List<String> morseWords) {
-
-        ArrayList<String> decodedSentence = morseWords.stream()
-                .map(MorseWordTranslator::translate)
-                .collect(Collectors.toCollection(ArrayList::new));
-
-        // Junta cada palavra da frase
-        return String.join(" ", decodedSentence);
+        return MorseSentenceTranslator.decodeMorseSentence(morseWords);
     }
 }
